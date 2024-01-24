8a12c3aa0726e62f3bda6088b8b9e56226875686
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2024-01-24 05:36:32 -0500

    Getting and also, almost getting the creating of Subject

--- src/pages/subject/index.jsx
+++ src/pages/subject/index.jsx
@@ -1,64 +1,98 @@
 import NavBar from '@/components/nav-bar';
 import SideBarMenu from '@/components/side-bar';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const host = 'http://localhost:8080';
 
 export default function SubjectPage() {
-  // Mock data for subjects
-  const subjects = [
-    {
-      id: 1,
-      code: 'ITEC 116',
-      description: 'IT ELECTIVE 4 (SYSTEM INTEGRATION AND ARCHITECTURE 2)',
-    },
-    { id: 2, code: 'ITEC 22A', description: 'COMPUTER PROGRAMMING 1' },
-    { id: 3, code: 'ITEC 22B', description: 'COMPUTER PROGRAMMING 2' },
-    // Add more subjects as needed
-  ];
-
-  // Mock data for student information
-  const studentInfo = {
-    instructor: 'John Doe',
-    subjectCode: 'ITEC 116',
-    subjectDescription: 'IT ELECTIVE 4 (SYSTEM INTEGRATION AND ARCHITECTURE 2)',
-    students: [
-      {
-        studentNumber: '202010383',
-        lastName: 'Buganan',
-        firstName: 'Anthony Monel',
-        middleName: 'Relies',
-        course: 'BSIT',
-      },
-      {
-        studentNumber: '202010382',
-        lastName: 'Claridad',
-        firstName: 'Jesusa',
-        middleName: 'Adlawan',
-        course: 'BSIT',
-      },
-      {
-        studentNumber: '202010381',
-        lastName: 'Cobreta',
-        firstName: 'Romeo',
-        middleName: 'Ceros',
-        course: 'BSIT',
-      },
-      {
-        studentNumber: '202010395',
-        lastName: 'Evangelista',
-        firstName: 'Angelo Gabriel',
-        middleName: 'Dillera',
-        course: 'BSIT',
-      },
-      // Add more students as needed
-    ],
-  };
-
+  const [subjects, setSubjects] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [isAddSubjectOpen, setIsAddSubjectOpen] = useState(false);
   const [isStudentInfoOpen, setIsStudentInfoOpen] = useState(false);
-  const [sections, setSections] = useState([]);
+  const [subjectCode, setSubjectCode] = useState('');
+  const [subjectDescription, setSubjectDescription] = useState('');
+  const fetchSubjects = async () => {
+    try {
+      setLoading(true);
+
+      const subjectsResponse = await fetch(`${host}/subjects`, {
+        credentials: 'include',
+      });
+
+      if (subjectsResponse.ok) {
+        const subjectsData = await subjectsResponse.json();
+        setSubjects(subjectsData);
+      } else {
+        showToast(`Error fetching subjects: ${subjectsResponse.statusText}`, false);
+      }
+    } catch (error) {
+      showToast(`Error fetching subjects: ${error}`, false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchSubjects();
+  }, []);
+
+  const handleLogin = async () => {
+    const formData = new URLSearchParams();
+    formData.append('username', 'admin');
+    formData.append('password', 'password');
+
+    try {
+      const response = await fetch(`${host}/auth/login`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: formData.toString(), // Convert formData to string
+      });
+
+      if (response.ok) {
+        console.log('Login successful');
+        getCurrentUser();
+      } else {
+        // Handle different HTTP status codes
+        if (response.status === 401) {
+          console.error('Authentication failed: Invalid credentials');
+        } else {
+          console.error('Login failed. Status:', response.status);
+        }
+      }
+    } catch (error) {
+      console.error('Error during login:', error.message);
+    }
+  };
+
+  const addSubject = async () => {
+    try {
+      const response = await fetch(`${host}/subjects`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          code: subjectCode,
+          description: subjectDescription,
+        }),
+      });
+
+      if (response.ok) {
+        alert('Subject added successfully');
+        closeAddSubject();
+        fetchSubjects(); // Refresh the subjects after adding a new one
+      } else {
+        alert(`Error adding subject: ${response.statusText}`);
+      }
+    } catch (error) {
+      alert(`Error adding subject: ${error}`);
+    }
+  };
 
   const openAddSubject = (e) => {
     e.stopPropagation();
@@ -90,31 +124,34 @@ export default function SubjectPage() {
       <div className="flex">
         <SideBarMenu />
         <div className="bg-gray-100 h-10/12 m-8 mb-4 flex w-full flex-col items-center justify-end">
-          <div className="border-black-500 relative w-3/4 flex-1 flex-grow overflow-hidden border-2 bg-white shadow-md">
-            <div className="border-b-2 bg-green p-2">
-              <h2 className="flex justify-center text-xl font-semibold text-yellow">
-                ALL SUBJECTS
-              </h2>
-            </div>
-            {subjects.map((subject) => (
-              <div
-                key={subject.id}
-                className="border-black-500 m-5 w-2/4 cursor-pointer rounded-lg border-2"
-                onClick={openSubjectInfo}
-              >
-                <div className="m-4">
-                  <h1 className="text-2xl font-semibold">{subject.code}</h1>
-                  <span>{subject.description}</span>
-                </div>
+          <div className="border-black-500 relative w-3/4 border-2 bg-white shadow-md">
+            <div className="overflow-y-scroll">
+              <div className="border-b-2 bg-green p-2">
+                <h2 className="flex justify-center text-xl font-semibold text-yellow">
+                  ALL SUBJECTS
+                </h2>
               </div>
-            ))}
-
-            <button
-              className="border-black-500 absolute bottom-0 right-0 mb-14 mr-14 w-48 border-2 p-2"
-              onClick={openAddSubject}
-            >
-              ADD SUBJECT
-            </button>
+              {subjects.map((subject) => (
+                <div
+                  key={subject.id}
+                  className="border-black-500 m-5 w-2/4 cursor-pointer rounded-lg border-2"
+                  onClick={() => openSubjectInfo(subject.id)}
+                >
+                  <div className="m-4">
+                    <h1 className="text-2xl font-semibold">{subject.code}</h1>
+                    <span>{subject.description}</span>
+                  </div>
+                </div>
+              ))}
+              <div>
+                <button
+                  className="border-black-500 absolute bottom-0 right-0 mb-14 mr-14 w-48 border-2 p-2"
+                  onClick={openAddSubject}
+                >
+                  ADD SUBJECT
+                </button>
+              </div>
+            </div>
           </div>
         </div>
         {isAddSubjectOpen && (
@@ -134,11 +171,15 @@ export default function SubjectPage() {
                         type="text"
                         className="m-1 w-3/4 rounded-lg bg-slate-200 p-1 font-semibold"
                         placeholder="Enter subject code"
+                        value={subjectCode}
+                        onChange={(e) => setSubjectCode(e.target.value)}
                       />
                       <input
                         type="text"
                         className="m-1 w-3/4 rounded-lg bg-slate-200 p-1 font-semibold"
                         placeholder="Enter subject description / name"
+                        value={subjectDescription}
+                        onChange={(e) => setSubjectDescription(e.target.value)}
                       />
                     </div>
                     <div className="m-5">
@@ -251,7 +292,9 @@ export default function SubjectPage() {
                     >
                       CANCEL
                     </button>
-                    <button className="m-4 w-1/6  bg-green px-6 text-yellow">SAVE</button>
+                    <button className="m-4 w-1/6  bg-green px-6 text-yellow" onClick={addSubject}>
+                      SAVE
+                    </button>
                   </div>
                 </div>
               </div>
@@ -275,21 +318,18 @@ export default function SubjectPage() {
                         type="text"
                         className="m-1 w-1/3 rounded-lg bg-slate-200 p-1 font-semibold"
                         placeholder="Instructor"
-                        defaultValue={studentInfo.instructor}
                       />
                       <label className="text-md ml-6 font-semibold">SUBJECT CODE</label>
                       <input
                         type="text"
                         className="m-1 w-1/3 rounded-lg bg-slate-200 p-1 font-semibold"
                         placeholder="ITEC 116"
-                        defaultValue={studentInfo.subjectCode}
                       />
                       <label className="text-md font-semibold">SUBJECT DESCRIPTION</label>
                       <input
                         type="text"
                         className="m-1 w-3/5 rounded-lg bg-slate-200 p-1 font-semibold"
                         placeholder="IT ELECTIVE 4 (SYSTEM INTEGRATION AND ARCHITECTURE 2)"
-                        defaultValue={studentInfo.subjectDescription}
                       />
                     </div>
                     <div className="m-5 flex justify-end">
@@ -302,7 +342,7 @@ export default function SubjectPage() {
                       <button className="w-4/3 m-4 rounded-lg bg-green px-6 py-1 text-yellow">
                         EDIT INFORMATION
                       </button>
-                    </div>{' '}
+                    </div>
                   </div>
                   <div className="m-5 flex justify-center">
                     <table className="border-gray-500 border-collapse border ">
@@ -316,19 +356,28 @@ export default function SubjectPage() {
                         </tr>
                       </thead>
                       <tbody>
-                        {studentInfo.students.map((student) => (
-                          <tr key={student.studentNumber}>
-                            <td className="border-gray-500 border p-2">{student.studentNumber}</td>
-                            <td className="border-gray-500 border p-2">{student.lastName}</td>
-                            <td className="border-gray-500 border p-2">{student.firstName}</td>
-                            <td className="border-gray-500 border p-2">{student.middleName}</td>
-                            <td className="border-gray-500 border p-2">{student.course}</td>
-                          </tr>
-                        ))}
+                        {/* {studentInfo.students.map((student) => (
+                            <tr key={student.studentNumber}>
+                              <td className="border-gray-500 border p-2">{student.studentNumber}</td>
+                              <td className="border-gray-500 border p-2">{student.lastName}</td>
+                              <td className="border-gray-500 border p-2">{student.firstName}</td>
+                              <td className="border-gray-500 border p-2">{student.middleName}</td>
+                              <td className="border-gray-500 border p-2">{student.course}</td>
+                            </tr>
+                          ))} */}
                       </tbody>
                     </table>
                   </div>
-                  <div className="mr-8 flex justify-end">{/* ... (your existing buttons) */}</div>
+                  <div className="mr-8 flex justify-end">
+                    {
+                      <button
+                        className="m-4 w-1/6 bg-green px-6 py-1  text-yellow"
+                        onClick={closeStudentInfo}
+                      >
+                        CANCEL
+                      </button>
+                    }
+                  </div>
                 </div>
               </div>
             </div>
